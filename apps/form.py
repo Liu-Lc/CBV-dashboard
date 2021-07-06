45f6a1a4305156a78449fbdb47d209268e7788cf
lcaliu23 <lucia.liu@utp.ac.pa>
2021-07-06 13:34:05 -0500

    Fixes in containers

--- apps/form.py
+++ apps/form.py
@@ -15,90 +15,53 @@ from app import app
 from dash.dependencies import Input, Output, State
 
 
-header = html.Div([html.Link(rel="stylesheet",
-        href="https://fonts.googleapis.com/css?family=Montserrat"),
-    html.Div(className='column', children=[
-        html.H1('Formulario'),
-        html.Hr()
-    ]),
-])
-
-form =  html.Div(className='container', children=[
-            html.Div(className='one-third column', children=[
-                html.Div(className='row', children=[
-                    html.Span('Apellidos', className='label'),
-                    html.Div(className='auto-column', children=[
-                        dcc.Input(),
-                        dcc.Input(),
-                    ])
-                ]),
-                html.Div(className='row', children=[
-                    html.Span('Nombre', className='label'),
-                    dcc.Input(),
-                ]),
-                html.Div(className='row', children=[
-                    html.Span('Cédula', className='label'),
-                    dcc.Input(),
-                ]),
-                html.Div(className='row', children=[
-                    html.Span('Fecha de nacimiento', className='label'),
-                    dcc.DatePickerSingle(className='input-style'),
-                ]),
-                html.P('', className='spacer'),
-                dcc.RadioItems(className='radio-items', id='search-type',
-                    options=[
-                        {'value':'ambigua', 'label':'Ambigua'},
-                        {'value':'precisa', 'label':'Precisa'},
-                        {'value':'exacta', 'label':'Exacta'},
-                    ], value='precisa'
-                ),
-                html.Button('BUSCAR', id='button-buscar', className='large-button'),
-            ]),
-            html.Div(className='two-thirds column table', children=[
-                table.DataTable(id='table-buscar',
-                    columns=[
-                        {'id':'apellido', 'name':'Apellido'},
-                        {'id':'nombre', 'name':'Nombre'},
-                        {'id':'cedula', 'name':'Cédula'},
-                        {'id':'fecha_nac', 'name':'Fecha de nacimiento'},
-                        {'id':'number', 'name':'No.'},
-                    ]
-                ),
-                html.Div(className='row', children=[
-                    html.Button('LIMPIAR', id='button-limpiar1', className='large-button'),
-                    html.Button('MODIFICAR', id='button-modificar1', className='large-button'),
-                    html.Button('ELIMINAR', id='button-eliminar1', className='large-button'),
-                ]),
+header = html.Div([
+            html.Link(rel="stylesheet",
+                href="https://fonts.googleapis.com/css?family=Montserrat"),
+            html.Link(rel="stylesheet",
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"),
+            html.Div(className='column', children=[
+                html.H1('Formulario'),
+                html.Hr()
             ]),
         ])
 
-form_add =  html.Div(className='container', children=[
+form =  html.Div(className='container', children=[
+            html.Div(className='row', children=[
                 html.Div(className='one-third column', children=[
                     html.Div(className='row', children=[
-                        html.Span('No.', className='label'),
-                        dcc.Input(),
-                    ]),
-                    html.Div(className='row', children=[
                         html.Span('Apellidos', className='label'),
-                        dcc.Input(),
+                        html.Div(className='auto-column', children=[
+                            dcc.Input(className='input-style'),
+                            dcc.Input(className='input-style'),
+                        ])
                     ]),
                     html.Div(className='row', children=[
                         html.Span('Nombre', className='label'),
-                        dcc.Input(),
+                        dcc.Input(className='input-style'),
                     ]),
                     html.Div(className='row', children=[
                         html.Span('Cédula', className='label'),
-                        dcc.Input(),
+                        dcc.Input(className='input-style'),
                     ]),
                     html.Div(className='row', children=[
-                        html.Span('Fecha de nacimiento'),
-                        dcc.DatePickerSingle(),
+                        html.Span('Fecha de nacimiento', className='label'),
+                        dcc.DatePickerSingle(id='fechanac1', className='input-style',
+                            clearable=True, 
+                        ),
                     ]),
                     html.P('', className='spacer'),
-                    html.Button('AGREGAR', id='button-agregar', className='large-button'),
+                    dcc.RadioItems(className='radio-items', id='search-option',
+                        options=[
+                            {'value':'ambigua', 'label':'Ambigua'},
+                            {'value':'precisa', 'label':'Precisa'},
+                            {'value':'exacta', 'label':'Exacta'},
+                        ], value='precisa'
+                    ),
                 ]),
+                ## Data Table
                 html.Div(className='two-thirds column table', children=[
-                    table.DataTable(
+                    table.DataTable(id='table-buscar',
                         columns=[
                             {'id':'apellido', 'name':'Apellido'},
                             {'id':'nombre', 'name':'Nombre'},
@@ -107,18 +70,69 @@ form_add =  html.Div(className='container', children=[
                             {'id':'number', 'name':'No.'},
                         ]
                     ),
-                    html.Div(className='row', children=[
-                        html.Button('LIMPIAR', id='button-limpiar2', className='large-button'),
-                        html.Button('MODIFICAR', id='button-modificar2', className='large-button'),
-                        html.Button('ELIMINAR', id='button-eliminar2', className='large-button'),
+                ]),
+            ]),
+            html.Div(className='button-container', children=[
+                html.Button('BUSCAR', id='button-buscar', className='large-button'),
+                html.Button('LIMPIAR', id='button-limpiar1', className='large-button'),
+                html.Button('MODIFICAR', id='button-modificar1', className='large-button'),
+                html.Button('ELIMINAR', id='button-eliminar1', className='large-button'),
+            ])
+        ])
+        
+
+form_add =  html.Div(className='container', children=[
+                html.Div(className='row', children=[
+                    html.Div(className='one-third column', children=[
+                        html.Div(className='row', children=[
+                            html.Span('No.', className='label'),
+                            dcc.Input(className='input-style'),
+                        ]),
+                        html.Div(className='row', children=[
+                            html.Span('Apellidos', className='label'),
+                            dcc.Input(className='input-style'),
+                        ]),
+                        html.Div(className='row', children=[
+                            html.Span('Nombre', className='label'),
+                            dcc.Input(className='input-style'),
+                        ]),
+                        html.Div(className='row', children=[
+                            html.Span('Cédula', className='label'),
+                            dcc.Input(className='input-style'),
+                        ]),
+                        html.Div(className='row', children=[
+                            html.Span('Fecha de nacimiento', className='label'),
+                            dcc.DatePickerSingle(id='fechanac2', className='input-style',
+                                clearable=True, 
+                            ),
+                        ]),
+                        html.P('', className='spacer'),
                     ]),
-                ])
+                    html.Div(className='two-thirds column table', children=[
+                        table.DataTable(
+                            columns=[
+                                {'id':'apellido', 'name':'Apellido'},
+                                {'id':'nombre', 'name':'Nombre'},
+                                {'id':'cedula', 'name':'Cédula'},
+                                {'id':'fecha_nac', 'name':'Fecha de nacimiento'},
+                                {'id':'number', 'name':'No.'},
+                            ]
+                        ),
+                    ]),
+                ]),
+                html.Div(className='button-container', children=[
+                    html.Button('AGREGAR', id='button-agregar', className='large-button'),
+                    html.Button('LIMPIAR', id='button-limpiar2', className='large-button'),
+                    html.Button('MODIFICAR', id='button-modificar2', className='large-button'),
+                    html.Button('ELIMINAR', id='button-eliminar2', className='large-button'),
+                ]),
             ])
 
 
 @app.callback(
     Output('table-buscar', 'data'),
-    [Input('button-buscar', 'n_clicks')]
+    [Input('button-buscar', 'n_clicks')],
+    [State('search-option', 'value')]
 )
-def button_buscar_click(nclick):
-    return {}
+def button_buscar_click(nclick, search_option):
+    return []
